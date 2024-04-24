7d4cedba8ee35904b422aeaff661878aec5f52d1
Nicholas <nicedson23@gmail.com>
2024-04-24 17:02:43 +0700

    add response dto and constant configs

---
+++ src/MidasAPI/PaginationDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace MidasAPI;
+
+public class PaginationDTO
+{
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalData { get; set; }
+    public int TotalPage
+    {
+        get  {
+            return (int)Math.Ceiling((double)TotalData / PageSize);
+        }
+    }
+}
