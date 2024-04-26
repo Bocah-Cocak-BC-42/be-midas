2613f1771a7134f2af661afa14aeb87fbded6113
Nicholas <nicedson23@gmail.com>
2024-04-26 14:48:02 +0700

    delete db connection

--- src/MidasDataAccess/DbContextConnectionConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MidasDataAccess;
-
-public static class DbContextConnectionConfig
-{
-    public static readonly string ConnectionString = "Data Source=LAPTOP-5P7RNJE4;Initial Catalog=Midas;Trusted_Connection=True;TrustServerCertificate=True";
-}
