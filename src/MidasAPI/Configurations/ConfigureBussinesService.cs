7667f2fe83b9d188074647ee2a9086c7861728cf
Nathan <nathanreinald@gmail.com>
2024-04-29 10:25:55 +0700

    add auth modul in configure business service

--- src/MidasAPI/Configurations/ConfigureBussinesService.cs
+++ src/MidasAPI/Configurations/ConfigureBussinesService.cs
@@ -7,9 +7,11 @@ public static class ConfigureBussinesService
     public static IServiceCollection AddBusinessServices(this IServiceCollection services){
         //Interfaces & Repositories
         services.AddScoped<IRoleRepository, RoleRepository>();
+        services.AddScoped<IAuthRepository, AuthRepository>();
 
         //Services
         services.AddScoped<RoleServices>();
+        services.AddScoped<AuthService>();
         
         return services;
     }
