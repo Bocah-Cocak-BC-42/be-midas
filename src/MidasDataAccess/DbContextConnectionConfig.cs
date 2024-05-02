5d1f61a479d01efdb65adc5b3cd72096a47f1208
Danny <livensiswoyo@gmail.com>
2024-05-02 14:06:36 +0700

    fixing user repo

--- src/MidasDataAccess/DbContextConnectionConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MidasDataAccess;
-
-public static class DbContextConnectionConfig
-{
-    public static readonly string ConnectionString = "Data Source=LAPTOP-GC0OOTL9;Initial Catalog=Midas;Trusted_Connection=True;TrustServerCertificate=True";
-}
