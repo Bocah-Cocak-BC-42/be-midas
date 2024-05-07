3c2f7b8450bf1fbd74e20d4f2cc3ef603f2b273b
Nathan <nathanreinald@gmail.com>
2024-05-07 08:55:06 +0700

    add custom validation annotation

--- src/MidasAPI/DTOs/Role/InsertRoleDTO.cs
+++ src/MidasAPI/DTOs/Role/InsertRoleDTO.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using MidasAPI.Validations.Role;
 
 namespace MidasAPI.DTOs.Role;
 
 public class InsertRoleDTO
 {   
+    [UniqueRoleInsertNameValidation]
     [Required(ErrorMessage = "Nama jabatan tidak boleh kosong!")]
     [RegularExpression(@"^[a-zA-Z''-'\s]*$", ErrorMessage = "Nama jabatan harus berupa karakter!")]
     public string Name { get; set; } = null!;
