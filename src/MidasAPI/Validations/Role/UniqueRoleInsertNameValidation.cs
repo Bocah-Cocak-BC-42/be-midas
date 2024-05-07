713a5471c39c769801e7874d1392cbececf8d4b1
Nathan <nathanreinald@gmail.com>
2024-05-07 08:50:04 +0700

    add new class for unique role name validation (upsert)

---
+++ src/MidasAPI/Validations/Role/UniqueRoleInsertNameValidation.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MidasDataAccess.Models;
+
+namespace MidasAPI.Validations.Role;
+
+public class UniqueRoleInsertNameValidation : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext){
+
+        if(value != null){
+            var dbContext = (MidasContext?) validationContext.GetService(typeof(MidasContext))
+                ?? throw new NullReferenceException("Kesalahan sistem");
+
+            var isNameExists = dbContext.Roles.Any(role => role.Name == value.ToString() && role.DeletedAt == null);
+
+            if(isNameExists){
+                return new ValidationResult($"Jabatan {value.ToString()} sudah terdaftar, tolong masukkan nama yang lain!");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
