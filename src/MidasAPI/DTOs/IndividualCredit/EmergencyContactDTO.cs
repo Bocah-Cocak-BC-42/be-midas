ad5ba294b40a8ed8713c25d4dd4838f6d27c304f
Rambel <rambelbastian26@gmail.com>
2024-05-08 11:05:56 +0700

    Add Personal Credit DTOs

---
+++ src/MidasAPI/DTOs/IndividualCredit/EmergencyContactDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace MidasAPI.DTO.IndividualCredit;
+
+public class EmergencyContactDTO
+{
+    public string Id { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+
+    public string IndividualCreditId { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string Relative { get; set; } = null!;
+}
