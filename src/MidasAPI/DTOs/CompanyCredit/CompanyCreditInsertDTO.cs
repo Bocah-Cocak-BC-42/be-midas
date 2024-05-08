88393f686631a1984534f8c1eb2070defbed15a8
Nathan <nathanreinald@gmail.com>
2024-05-08 16:12:10 +0700

    add validation for company credit insert

--- src/MidasAPI/DTOs/CompanyCredit/CompanyCreditInsertDTO.cs
+++ src/MidasAPI/DTOs/CompanyCredit/CompanyCreditInsertDTO.cs
@@ -1,8 +1,20 @@
-﻿namespace MidasAPI.DTOs.CompanyCredit;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MidasAPI.DTOs.CompanyCredit;
 
 public class CompanyCreditInsertDTO
-{
+{   
+
+    //Valdasi unique
+    [Required(ErrorMessage = "{0} tidak boleh kosong")]
+    [Range(1, Int64.MaxValue, ErrorMessage ="{0} harus berupa angka")]
+    [Length(16, 16, ErrorMessage = "{0} harus 16 karakter")]
+    [Display(Name = "NPWP Badan Usaha")]
     public string Npwp { get; set; } = null!;
+
+
+
+    [Required(ErrorMessage = "")]
     public string CompanyName { get; set; } = null!;
     public string CompanyType { get; set; } = null!;
     public string PlaceOfEstasblishment { get; set; } = null!;
