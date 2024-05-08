b8d98fab2d2bee63b7082de3eef5c72ae719197c
Vina <alvinanurfadilah@gmail.com>
2024-05-08 13:38:23 +0700

    add insert company credit

---
+++ src/MidasAPI/DTOs/CompanyCredit/CompanyCreditInsertDTO.cs
@@ -0,0 +1,32 @@
+﻿namespace MidasAPI.DTOs.CompanyCredit;
+
+public class CompanyCreditInsertDTO
+{
+    public string Npwp { get; set; } = null!;
+    public string CompanyName { get; set; } = null!;
+    public string CompanyType { get; set; } = null!;
+    public string PlaceOfEstasblishment { get; set; } = null!;
+    public string EstablishRegistrationNumber { get; set; } = null!;
+    public DateOnly EstablishRegistrationDate { get; set; }
+    public string CompanyRegistrationNumber { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string Address { get; set; } = null!;
+    public string VillageId { get; set; } = null!;
+    public string CompanyOwnerId { get; set; } = null!;
+    public string BranchOfficeId { get; set; } = null!;
+    public decimal ApplicationAmount { get; set; }
+    public int ApplicationPeriod { get; set; }
+    public DateTime? ApplicationDate { get; set; }
+    public DateTime CreditEndDate { get; set; }
+    public string Status { get; set; } = null!;
+    public string Notes { get; set; } = null!;
+    public string EstablishRegistrationNumberFile { get; set; } = null!;
+    public string CompanyRegistrationNumberFile { get; set; } = null!;
+    public string Npwpfile { get; set; } = null!;
+    public string IdentityNumberFile { get; set; } = null!;
+    public string BoardOfManagementFile { get; set; } = null!;
+    public string FinancialStatementFile { get; set; } = null!;
+
+    public BranchOfficeResponseDTO BranchOffice { get; set; }
+}
---
+++ src/MidasAPI/Services/CompanyCreditService.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MidasAPI.DTOs.CompanyCredit;
+using MidasAPI.DTOs.User;
+using MidasBussines.Interfaces;
+using MidasDataAccess.Models;
+
+namespace MidasAPI.Services;
+
+public class CompanyCreditService
+{
+    private readonly ICompanyCreditRepository _repository;
+    private readonly IBranchOfficeRepository _branchOfficeRepository;
+
+    public CompanyCreditService(ICompanyCreditRepository repository, IBranchOfficeRepository branchOfficeRepository)
+    {
+        _repository = repository;
+        _branchOfficeRepository = branchOfficeRepository;
+    }
+
+    private string CreateCreditApplicationNumber(string branchOfficeCode, string nik)
+    {
+        return $"KBU-{branchOfficeCode}-{nik}-{DateTime.Now.ToString("ddMMyyyy")}";
+    }
+
+    private List<SelectListItem> GetCompanyTypes()
+    {
+        return new List<SelectListItem>()
+        {
+            new SelectListItem() { Text = "Perseroan Terbatas (PT)", Value = "PT" },
+            new SelectListItem() { Text = "Commanditaire Vennootschap", Value = "CV" },
+            new SelectListItem() { Text = "Koperasi", Value = "Koperasi" },
+            new SelectListItem() { Text = "Koperasi Unit Desa (KUD)", Value = "KUD" },
+            new SelectListItem() { Text = "Perusahaan Dagang", Value = "PD" }
+        };
+    }
+
+    public void Insert(CompanyCreditInsertDTO dto, UserResponseDTO user)
+    {
+        var companyCredit = new CompanyCredit()
+        {
+            CreditApplicationNumber = CreateCreditApplicationNumber(dto.BranchOffice.Code, user.IdentityNumber),
+            Npwp = dto.Npwp,
+            CompanyName = dto.CompanyName,
+            CompanyType = dto.CompanyType,
+            PlaceOfEstasblishment = dto.PlaceOfEstasblishment,
+            EstablishRegistrationNumber = dto.EstablishRegistrationNumber,
+            EstablishRegistrationDate = dto.EstablishRegistrationDate,
+            CompanyRegistrationNumber = dto.CompanyRegistrationNumber,
+            Email = dto.Email,
+            PhoneNumber = dto.PhoneNumber,
+            Address = dto.Address,
+            VillageId = dto.VillageId,
+            CompanyOwnerId = dto.CompanyOwnerId,
+            BranchOfficeId = dto.BranchOfficeId,
+            ApplicationAmount = dto.ApplicationAmount,
+            ApplicationPeriod = dto.ApplicationPeriod,
+            ApplicationDate = dto.ApplicationDate,
+            CreditEndDate = dto.CreditEndDate,
+            Status = dto.Status,
+            Notes = dto.Notes,
+            EstablishRegistrationNumberFile = dto.EstablishRegistrationNumberFile,
+            CompanyRegistrationNumberFile = dto.CompanyRegistrationNumberFile,
+            Npwpfile = dto.Npwpfile,
+            IdentityNumberFile = dto.IdentityNumberFile,
+            BoardOfManagementFile = dto.BoardOfManagementFile,
+            FinancialStatementFile = dto.FinancialStatementFile,
+            CreatedBy = user.Id,
+            CreatedAt = DateTime.Now
+        };
+
+        _repository.Insert(companyCredit);
+    }
+}
