4ae39f546f39ad96e5bc67491e63c91a5d95ed1e
Nathan <nathanreinald@gmail.com>
2024-05-10 16:23:37 +0700

    add new company credit DTO

---
+++ src/MidasAPI/DTOs/CompanyCredit/CompanyCreditDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MidasAPI.DTOs.CompanyCredit;
+
+public class CompanyCreditDTO
+{
+    public string CreditApplicationNumber { get; set; } = null!;
+    public string BranchOffice { get; set; } = null!;
+    public decimal ApplicationAmount { get; set; }
+    public int ApplicationPeriod { get; set; }
+    public string Status { get; set; } = null!;
+}
