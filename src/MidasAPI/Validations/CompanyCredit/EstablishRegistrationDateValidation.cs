03c9efd233c7c4816906de35d7a6f104ea288a4e
Nathan <nathanreinald@gmail.com>
2024-05-10 16:23:25 +0700

    add new class for company credit date validation

---
+++ src/MidasAPI/Validations/CompanyCredit/EstablishRegistrationDateValidation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MidasAPI.Validations.CompanyCredit;
+
+public class EstablishRegistrationDateValidation : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext){
+
+        if(value != null){
+            DateOnly val = (DateOnly)value;
+
+            if(val > DateOnly.FromDateTime(DateTime.Now)){
+                return new ValidationResult("Tanggal Akta Pendirian tidak bisa melebihi tanggal hari ini");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+
+}
